d5880ea918f8c8570a050f9ca8dc2c6733b18f8f
vakalapa <vakalapa@cisco.com>
2017-05-15 11:04:51 -0400

    new deploy script

--- deploy.cmd
+++ deploy.cmd
@@ -2,7 +2,7 @@
 
 :: ----------------------
 :: KUDU Deployment Script
-:: Version: 1.0.9
+:: Version: 1.0.8
 :: ----------------------
 
 :: Prerequisites
@@ -94,58 +94,13 @@ IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
 IF NOT EXIST "%DEPLOYMENT_TARGET%\requirements.txt" goto postPython
 IF EXIST "%DEPLOYMENT_TARGET%\.skipPythonDeployment" goto postPython
 
-echo Detected requirements.txt.  You can skip Python specific steps with a .skipPythonDeployment file.
+echo Detected requirements.txt. RUNNING CUSTOM DEPLOYMENT
 
-:: 2. Select Python version
-call :SelectPythonVersion
-
-pushd "%DEPLOYMENT_TARGET%"
-
-:: 3. Create virtual environment
-IF NOT EXIST "%DEPLOYMENT_TARGET%\env\azure.env.%PYTHON_RUNTIME%.txt" (
-  IF EXIST "%DEPLOYMENT_TARGET%\env" (
-    echo Deleting incompatible virtual environment.
-    rmdir /q /s "%DEPLOYMENT_TARGET%\env"
-    IF !ERRORLEVEL! NEQ 0 goto error
-  )
-
-  echo Creating %PYTHON_RUNTIME% virtual environment.
-  %PYTHON_EXE% -m %PYTHON_ENV_MODULE% env
-  IF !ERRORLEVEL! NEQ 0 goto error
-
-  copy /y NUL "%DEPLOYMENT_TARGET%\env\azure.env.%PYTHON_RUNTIME%.txt" >NUL
-) ELSE (
-  echo Found compatible virtual environment.
-)
-
-:: 4. Install packages
+:: 2. Install packages
 echo Pip install requirements.
-env\scripts\pip install -r requirements.txt
+D:\home\Python27\python.exe -m pip install --upgrade -r requirements.txt
 IF !ERRORLEVEL! NEQ 0 goto error
 
-REM Add additional package installation here
-REM -- Example --
-REM env\scripts\easy_install pytz
-REM IF !ERRORLEVEL! NEQ 0 goto error
-
-:: 5. Copy web.config
-IF EXIST "%DEPLOYMENT_SOURCE%\web.%PYTHON_VER%.config" (
-  echo Overwriting web.config with web.%PYTHON_VER%.config
-  copy /y "%DEPLOYMENT_SOURCE%\web.%PYTHON_VER%.config" "%DEPLOYMENT_TARGET%\web.config"
-)
-
-:: 6. Django collectstatic
-IF EXIST "%DEPLOYMENT_TARGET%\manage.py" (
-  IF EXIST "%DEPLOYMENT_TARGET%\env\lib\site-packages\django" (
-    IF NOT EXIST "%DEPLOYMENT_TARGET%\.skipDjango" (
-      echo Collecting Django static files. You can skip Django specific steps with a .skipDjango file.
-      IF NOT EXIST "%DEPLOYMENT_TARGET%\static" (
-        MKDIR "%DEPLOYMENT_TARGET%\static"
-      )
-      env\scripts\python manage.py collectstatic --noinput --clear
-    )
-  )
-)
 
 popd
 
